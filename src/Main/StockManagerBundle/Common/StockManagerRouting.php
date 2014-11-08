9ea3a9f682ca2338a47c9ff00cfecb892b1b2971
alduleacristi31 <alduleacristi31@gmail.com>
2014-11-08 15:53:16 +0000

    am adaugat url-uri noi pentru insert_product si insert_producer

--- src/Main/StockManagerBundle/Common/StockManagerRouting.php
+++ src/Main/StockManagerBundle/Common/StockManagerRouting.php
@@ -3,11 +3,17 @@ namespace Main\StockManagerBundle\Common;
 
 class StockManagerRouting{
 	const HOME_URL = "/index";
+	const HOME_ADMIN_URL = "/admin/index";
 	const ABOUT_URL = "/about";
-	const INSERT_CATEGORY_URL = "/insert/category";
+	const INSERT_CATEGORY_URL = "/admin/insert/category";
+	const INSERT_PRODUCT_URL = "/admin/insert/product";
+	const INSERT_PRODUCER_URL = "/admin/insert/producer";
 
 	const HOME_KEY = "main_stock_manager_homepage";
+	const HOME_ADMIN_KEY = "main_stock_manager_admin_homepage";
 	const ABOUT_KEY = "main_stock_manager_about";
 	const INSERT_CATEGORY_KEY = "main_stock_manager_insert_category";
+	const INSERT_PRODUCT_KEY = "main_stock_manager_insert_product";
+	const INSERT_PRODUCER_KEY = "main_stock_manager_insert_producer";
 } 
 ?>
